3ea47e1b8c25c72f32f2eb0310b3364b39d1a903
rahmaaroua <98782603+rahmaaroua@users.noreply.github.com>
2025-01-25 13:35:55 +0100

    Update  TaskManager.java

---
+++ JAVA_RMI/TaskManager.java
@@ -0,0 +1,30 @@
+package JAVA_RMI;
+
+import java.util.ArrayList;
+
+public class TaskManager implements TaskManagerInterface{
+
+
+    private final ArrayList<String> tasks=new ArrayList<>();
+
+    @Override
+    public String addTask(String task){
+        tasks.add(task);
+        return "Task added successfully";
+    }
+
+    @Override
+    public String removeTask(Integer id) {
+     if (tasks.size()-id>0) {
+         tasks.remove(tasks.get(id));
+         return "Task removed successfully";}
+     else {
+         return "Task not found";
+     }
+    }
+    @Override
+    public ArrayList<String> getTasks() {
+        return tasks;
+
+    }
+}
